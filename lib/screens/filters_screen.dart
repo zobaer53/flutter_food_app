e4ca01477fc2f430191796b51ce28db0173c0025
zobaer53 <hossain.zobaer11858@gmail.com>
2024-01-12 19:24:36 +0600

    feature -- replace screen

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:meal_management/screens/tabs_screen.dart';
+import 'package:meal_management/widget/main_drawer.dart';
 
 class FiltersScreen extends StatefulWidget {
   @override
@@ -9,36 +11,63 @@ class FiltersScreen extends StatefulWidget {
 
 }
 
-class _FilterScreenState extends State<FiltersScreen>{
+class _FilterScreenState extends State<FiltersScreen> {
   var _glutenFreeFilterSet = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(
-        'Your Filters'
+          'Your Filters'
       ),
       ),
+      drawer: MainDrawer(onSelectScreen: (identifier) {
+        Navigator.of(context).pop();
+        if(identifier == 'meals'){
+          Navigator.push(context, MaterialPageRoute(builder: (ctx) => TabsScreen()));
+        }
+      },),
       body: Column(
         children: [
-          SwitchListTile(value: _glutenFreeFilterSet,
-            onChanged:(isChecked){ setState(() {
-              _glutenFreeFilterSet = isChecked;
-            }); },
-          title: Text('Gluten-free',
-            style: Theme.of(context).textTheme.titleLarge!.copyWith(
-            color: Theme.of(context).colorScheme.onBackground
-          )
-            ,),
+          SwitchListTile(
+            value: _glutenFreeFilterSet,
+            onChanged: (isChecked) {
+              setState(() {
+                _glutenFreeFilterSet = isChecked;
+              });
+            },
+            title: Text('Gluten-free',
+              style: Theme
+                  .of(context)
+                  .textTheme
+                  .titleLarge!
+                  .copyWith(
+                  color: Theme
+                      .of(context)
+                      .colorScheme
+                      .onBackground
+              )
+              ,),
             subtitle: Text('Only include gluten-free meals',
-                style: Theme.of(context).textTheme.labelMedium!.copyWith(
-                    color: Theme.of(context).colorScheme.onBackground
+                style: Theme
+                    .of(context)
+                    .textTheme
+                    .labelMedium!
+                    .copyWith(
+                    color: Theme
+                        .of(context)
+                        .colorScheme
+                        .onBackground
                 )
             ),
-            activeColor: Theme.of(context).colorScheme.tertiary,
+            activeColor: Theme
+                .of(context)
+                .colorScheme
+                .tertiary,
             contentPadding: EdgeInsets.only(left: 34, right: 22),
           )
         ],
       ),
     );
   }
-
+}
--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -64,10 +64,9 @@ class _TabsScreenState extends State<TabsScreen> {
   }
 
   void _setScreen(String identifier) {
+    Navigator.of(context).pop();
     if (identifier == 'filters') {
       Navigator.push(context, MaterialPageRoute(builder: (ctx) => FiltersScreen()));
-    } else {
-      Navigator.of(context).pop();
     }
   }
 
@@ -91,7 +90,8 @@ class _TabsScreenState extends State<TabsScreen> {
         title: Text(activePageTitle),
       ),
       drawer: MainDrawer(
-        onSelectScreen: _setScreen,
+        onSelectScreen:
+        _setScreen,
       ),
       body: activePage,
       bottomNavigationBar: BottomNavigationBar(
