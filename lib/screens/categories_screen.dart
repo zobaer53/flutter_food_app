8b52e1d412ae7da5d671765b074d0cda177eab59
zobaer53 <hossain.zobaer11858@gmail.com>
2024-01-25 16:50:01 +0600

    feature -- explicit animation added

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -6,14 +6,41 @@ import 'package:meal_management/widget/category_grid_item.dart';
 import '../model/category.dart';
 import '../model/meal.dart';
 
-class CategoriesScreen extends StatelessWidget{
+class CategoriesScreen extends StatefulWidget{
   const CategoriesScreen({super.key, required this.availableMeal});
 
   final  List<Meal> availableMeal;
 
-  // navigation with the selected item
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+/* explicit animation setup
+* manually control animation state
+* more control but complex*/
+class _CategoriesScreenState extends State<CategoriesScreen> with SingleTickerProviderStateMixin{
+ late  AnimationController _animationController;
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(vsync: this,
+    duration: Duration(milliseconds: 300),
+      lowerBound: 0,
+      upperBound: 1,
+    );
+
+    _animationController.forward();
+  }
+
+
+ @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+//
+  } // navigation with the selected item
   void _selectCategory(BuildContext context, Category category){
-    final filteredMeals = availableMeal.where((meal) => meal.categories.contains(category.id)).toList();
+    final filteredMeals = widget.availableMeal.where((meal) => meal.categories.contains(category.id)).toList();
     Navigator.push(context,MaterialPageRoute(builder: (ctx) => MealsScreen(meals: filteredMeals, title: category.title))); //keeps screen in backstack
     /*Navigator.pushNamed(context, )*/
   }
@@ -22,9 +49,10 @@ class CategoriesScreen extends StatelessWidget{
   Widget build(BuildContext context) {
     /*
       GridView comes with normal and performance optimized GridView.builder()
-
        */
-    return  GridView(
+    return AnimatedBuilder(
+      animation: _animationController,
+      child: GridView(
         padding: const EdgeInsets.all(24),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2, //controls the layout of gridView,
@@ -39,7 +67,18 @@ class CategoriesScreen extends StatelessWidget{
             CategoryGridItem(category: category,setCategory: (){
               _selectCategory(context,category);
             },),
-      ],
+        ],
+      ),
+      builder: (ctx,child) {
+        final  paddingValue = 100 - _animationController.value * 100;
+        return Padding(
+            padding:  EdgeInsets.only(
+              top: paddingValue,
+        ),
+          child: child);
+      }
       );
+
+
   }
 }
